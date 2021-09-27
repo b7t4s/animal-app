718b956c41ec51c4b15df25fbcedc330c7f04749
宮川郁香 <miyakawaayaka@miyagawakaorukounoMacBook-Pro.local>
2021-09-27 23:12:36 +0900

    ファイルにデータを保存する

--- index.php
+++ index.php
@@ -1,14 +1,35 @@
 <?php 
 
+//メッセージを保存するファイルのパス設定
+define('FILENAME', './message.txt');
+
+//タイムゾーン設定
+date_default_timezone_set('Asia/Tokyo');
+
 if(!empty($_POST['btn_submit'])) {
-    var_dump($_POST);
+
+    if($file_handle = fopen( FILENAME,"a")) {
+
+        //書き込み日時を取得
+        $current_date = date("Y-m-d H:i:s");
+
+        //書き込むデータを作成
+        $data = "'".$_POST['view_name']."','".$_POST['message']."','".$current_date."'\n";
+
+        //書き込み
+        fwrite($file_handle,$data);
+
+        //ファイルを閉じる
+        fclose($file_handle);
+    }
+    //var_dump($_POST);
 }
 
 ?>
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
