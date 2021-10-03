ebd6393bfff21ede2468ef0475fcc2c9c577f865
金城弘樹 <kinjouhiroki@mb-air.local>
2021-10-03 15:59:22 +0900

    画面モック追加、スペルミス修正

--- dbc.php
+++ dbc.php
@@ -4,70 +4,72 @@
 //②foreachで表示
 //-出力はエスケープする
 
-    function dbc() {
-        $host = "localhost";
-        $dbname = "animal-app";
-        $user = "root";
-        $pass = "root";
+function dbc()
+{
+    $host = "localhost";
+    $dbname = "animal-app";
+    $user = "root";
+    $pass = "root";
 
-        $dns = "mysql:host=$host;dbname=$dbname;charset=utf8";
+    $dns = "mysql:host=$host;dbname=$dbname;charset=utf8";
 
-        try{
-            $pdo = new PDO($dns,$user,$pass,
+    try {
+        $pdo = new PDO($dns, $user, $pass,
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             ]);
-            return $pdo;
-        }catch(PDOException $e){
-            exit($e->getMessage());
-        }
-        
+        return $pdo;
+    } catch (PDOException $e) {
+        exit($e->getMessage());
     }
 
-    /**
-     * ファイルデータを保存
-     * @param string $filename ファイル名
-     * @param string $save_path 保存先のパス
-     * @param string $caption 投稿の説明
-     * @return bool $result
-     */
-    function fileSave($filename,$save_path,$caption) {
-        $result = false;
+}
 
-        $sql = "INSERT INTO file_table(file_name,file_path,caption)VALUES(?,?,?)";
+/**
+ * ファイルデータを保存
+ * @param string $filename ファイル名
+ * @param string $save_path 保存先のパス
+ * @param string $caption 投稿の説明
+ * @return bool $result
+ */
+function fileSave($filename, $save_path, $caption)
+{
+    $result = false;
 
-        try{
-            $stmt = dbc()->prepare($sql);//SQLの準備
+    $sql = "INSERT INTO file_table(file_name,file_path,caption)VALUES(?,?,?)";
 
-            $stmt->bindValue(1,$filename);//？に三つ入れる
-            $stmt->bindValue(2,$save_path);
-            $stmt->bindValue(3,$caption);
+    try {
+        $stmt = dbc()->prepare($sql); //SQLの準備
 
-            $result = $stmt->execute();//SQL文を実行
-            return $result;
+        $stmt->bindValue(1, $filename); //？に三つ入れる
+        $stmt->bindValue(2, $save_path);
+        $stmt->bindValue(3, $caption);
 
-        }catch(\Exception $e){
-            echo $e->getMessage();
-            return $result;
-        }
+        $result = $stmt->execute(); //SQL文を実行
+        return $result;
+
+    } catch (\Exception $e) {
+        echo $e->getMessage();
+        return $result;
     }
+}
 
-    /**
-     * ファイルデータを保存
-     * @return array $fileData
-     */
-        
-     function getAllFile() {
-         $sql = "SELECT * FROM file_table";
+/**
+ * ファイルデータを保存
+ * @return array $fileData
+ */
 
-         $fileData = dbc()->query($sql);
+function getAllFile()
+{
+    $sql = "SELECT * FROM file_table order by id desc";
 
-         return $fileData;
-     }
-    
-     function h($s) {
-         return htmlspecialchars($s,ENT_QUOTES,"UTF-8");
-     }
+    $fileData = dbc()->query($sql);
 
-?>
+    return $fileData;
+}
+
+function h($s)
+{
+    return htmlspecialchars($s, ENT_QUOTES, "UTF-8");
+}
