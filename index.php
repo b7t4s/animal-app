cd8823ac3c58239d80f444cdeed962b8bc448461
宮川郁香 <miyakawaayaka@miyagawakaorukounoMacBook-Pro.local>
2021-10-05 19:07:17 +0900

    投稿の未入力バリデーションをつける

--- index.php
+++ index.php
@@ -14,24 +14,39 @@ $split_data = null;
 $message = array();
 $message_array = array();
 $success_message = null;
+$error_message = array();
 
 if(!empty($_POST['btn_submit'])) {
 
-    if($file_handle = fopen( FILENAME,"a")) {
+    //名前の入力チェック
+    if(empty($_POST['view_name'])) {
+        $error_message[] = '表示名を入力してください。';
+    }
 
-        //書き込み日時を取得
-        $current_date = date("Y-m-d H:i:s");
+    //メッセージの入力チャック
+    if(empty($_POST['message'])) {
+        $error_message[] = 'メッセージを入力してください。';      
+    }
 
-        //書き込むデータを作成
-        $data = "'".$_POST['view_name']."','".$_POST['message']."','".$current_date."'\n";
+    if(empty($error_message)) {
 
-        //書き込み
-        fwrite($file_handle,$data);
+        if($file_handle = fopen( FILENAME,"a")) {
 
-        //ファイルを閉じる
-        fclose($file_handle);
+            //書き込み日時を取得
+            $current_date = date("Y-m-d H:i:s");
+    
+            //書き込むデータを作成
+            $data = "'".$_POST['view_name']."','".$_POST['message']."','".$current_date."'\n";
+    
+            //書き込み
+            fwrite($file_handle,$data);
+    
+            //ファイルを閉じる
+            fclose($file_handle);
+    
+            $success_message = 'メッセージを書き込みました。';
 
-        $success_message = 'メッセージを書き込みました。';
+        }  
     }
 }
 
@@ -67,6 +82,13 @@ if($file_handle = fopen(FILENAME,'r')) {
     <form method="post">
         <?php if(!empty($success_message)): ?>
             <p class="success_message"><?php echo $success_message; ?></p>
+        <?php endif; ?>
+        <?php if(!empty($error_message)): ?>
+            <ul class="error_message">
+                <?php foreach($error_message as $value): ?>
+                    <li>・<?php echo $value; ?></li>
+                <?php endforeach; ?>
+            </ul>
         <?php endif; ?>
         <div>
             <label for="view_name">名前</label>
